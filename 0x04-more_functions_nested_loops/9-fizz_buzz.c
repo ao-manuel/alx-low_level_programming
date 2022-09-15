6f3afd4b7221704dbc6716a1879619ec785998e1
ao-manuel <afolayanemmanuel18@yahoo.com>
2022-09-15 23:26:54 +0000

    [Fix]: Fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,17 +17,17 @@ int main(void)
 	printf("%d", x);
 	for (x = 2; x <= 100; x++)
 	{
-		if ((x % 3) == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (x % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if ((x % 5) == 0)
+		else if (x % 5 == 0)
 		{
 			printf(" Buzz");
-		}
-		else if ((x % 15) == 0)
-		{
-			printf(" FizzBuzz");
 		}
 		else
 		{
