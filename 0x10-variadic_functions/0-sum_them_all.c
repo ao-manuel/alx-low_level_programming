118cac64ce3f0bb07a47e3f936e1eddd5afd0083
ao-manuel <afolayanemmanuel18@yahoo.com>
2022-10-13 21:33:51 +0100

    Sum args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,14 +11,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int sum = 0;
+	va_list ap;
+
 	if (n == 0)
 		return (0);
-	int sum, i;
-	va_list ap;
 
 	va_start(ap, n);
 
-	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
